ae89d3ecc0930b6197bfe50bac27db22f79dc925
sm7177 <subrinamaharjan2020@gmail.com>
2024-09-22 16:01:06 +0545

    python-operators and datatypes

---
+++ main.py
@@ -0,0 +1,44 @@
+# Activity 1
+# name="Ram"
+# age=30
+# status=True
+# weight=50.8
+
+# print("My name is ",name)
+# print(type(name))
+# print(type(age))
+# print(type(status))
+# print(type(weight))
+
+# new_age=str(age)
+# print(type(new_age))
+
+# Activity 2
+# num1=20
+# num2=40
+
+# print("Addition",num1+num2)
+# print("Subtraction",num1-num2)
+# print("Product",num1*num2)
+# print("Division",num1/num2)
+# print("Floor Division",num1//num2)
+# print(num1**2)
+
+# print(num1>num2)
+# print(num1==num2)
+# print(num1<=num2)
+# print(num1!=num2)
+
+# Activity 3
+first_name="Republic"
+last_name="Dominick"
+full_name=first_name+" "+last_name
+print(full_name)
+
+ex="hello"*6
+print(ex)
+
+print(len(first_name))
+print(first_name[3])
+print(first_name[-1])
+print(first_name[2:5])
